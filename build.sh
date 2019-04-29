eb01941fa1f3f2317285d3d9d301fc5b2e29a3f2
litao1104 <litao1104@gmail.com>
2019-04-29 10:53:03 +0800

    build: modify defconfig name

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ export SUBARCH=arm64
 export DTC_EXT=$DTC_DIR/dtc
 export CROSS_COMPILE=$GCC_DIR/bin/aarch64-linux-android-
 
-make O=out REAL_CC=$LLVM_DIR/toolchains/llvm-Snapdragon_LLVM_for_Android_6.0/prebuilt/linux-x86_64/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- cepheus_defconfig
+make O=out REAL_CC=$LLVM_DIR/toolchains/llvm-Snapdragon_LLVM_for_Android_6.0/prebuilt/linux-x86_64/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- cepheus_p_bawan_defconfig
 make O=out REAL_CC=$LLVM_DIR/toolchains/llvm-Snapdragon_LLVM_for_Android_6.0/prebuilt/linux-x86_64/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- -j4
 
 cp $KERNEL_DIR/out/arch/arm64/boot/Image-dtb $AIK_DIR/split_img/boot.img-zImage
